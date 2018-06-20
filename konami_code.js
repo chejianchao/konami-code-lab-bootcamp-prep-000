34a1a6db5b9014f6acfb092c331c62e5b61fd153
chejianchao <820992376@qq.com>
2018-06-20 04:42:28 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -13,6 +13,7 @@ const codes = [
 
 function init() {
   // your code here
+<<<<<<< HEAD
   var index = 0;
   document.body.addEventListener('keydown', function(e) {
     var len = codes.length
@@ -23,5 +24,11 @@ function init() {
     }else
       index = 0;
     
+=======
+  document.body.addEventListener('keydown', function(e) {
+    console.log(e.key)
+    if( codes.indexOf(e.key)>=0)
+      alert(e.key)
+>>>>>>> 9d470d3b37ffb176962dc6279841ea136fe4f541
   })
 }
